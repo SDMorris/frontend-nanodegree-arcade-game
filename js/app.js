a23d16960e88992beef94e0af8def774cfc28652
SDMorris <sdmorris203@gmail.com>
2015-08-26 21:12:50 -0700

    Changes created to comply with assignment
    The changes address the requirements for assignment 3.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
+
+//collision detection
+var iscolide = function(obj1x, obj1y, obj2x, obj2y, range){
+  if (((obj1x + range) >= obj2x) && ((obj1x - range) <= obj2x ) && ((obj1y + range) >= obj2y) && ((obj1y - range) <= obj2y) ){
+  console.log(obj1x, obj1y, obj2x, obj2y, range);
+  return true;
+  }
+}
+
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(loc,speed) {
+  this.x = 0;
+  this.y= loc;
+  this.speed = speed;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -7,30 +20,103 @@ var Enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 }
-
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-}
+    //check for collision
+    if (iscolide(this.x, this.y, player.x, player.y, 50)){
+    this.x=0;
+    player.y = 400;
+    player.x = 200;
 
+    }
+        this.x = this.x + (dt * this.speed);
+
+    if ( this.x > 490 ) {
+      this.x = 0;
+    }
+}
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
-
-// Now write your own player class
+//Enemy.prototype.constuctor = Enemy;
+// Now write your own layer class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.speed = 81;
+  this.x = 200;
+  this.y = 400;
+  this.sprite = 'images/char-boy.png';
+}
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.dt = dt;
+}
 
+Player.prototype.handleInput= function(keycode){
+  //37: 'left',
+  //38: 'up',
+  //39: 'right',
+  //40: 'down'
+  switch ( keycode ){
+    case 'left':
+      if (this.x == 0) {
+        break;
+      }
+      this.x = this.x - 100;
+      break;
+
+    case 'up':
+      if (this.y == -5  ) {
+        this.y = 400;
+        this.x = 200;
+        break;
+      }
+      this.y = this.y - (this.speed);
+      break;
+
+    case 'right':
+      if (this.x == 400){
+        break;
+      }
+      this.x = this.x + 100;
+      break;
+
+    case 'down':
+      if (this.y == 400){
+        break;
+      }
+      this.y = this.y + (this.speed);
+      break;
+    }
+}
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [];
+var enemy = new Enemy(225, 300);
+allEnemies.push( enemy );
+var enemy2 = new Enemy (150, 200 );
+allEnemies.push( enemy2 );
+var enemy3 = new Enemy(75, 150);
+allEnemies.push( enemy3 );
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
